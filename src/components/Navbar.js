92b8ba657532b3b3eb95a797fc80d8d8381fb874
Brooke Love <savienloveb@gmail.com>
2022-08-05 16:47:03 -0700

    create folders and made landing navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Navbar() {
+  const [checked, setChecked] = React.useState(false);
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+  return (
+    <div>
+      <h3>
+        <a>Instagram</a>
+      </h3>
+      <h3>
+        <a>Instagram</a>
+      </h3>
+      <h3>
+        <a>Cash App</a>
+      </h3>
+      <h3>
+        <a>About</a>
+      </h3>
+      <h3>
+        <a>Contact</a>
+      </h3>
+      <input type="checkbox" checked={checked} onChange={handleChange} />
+    </div>
+  );
+}
---
+++ src/pages/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+export default function Landing() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Landing</h1>
+      <h1>Products</h1>
+      <h1>About</h1>
+      <h1>Contact</h1>
+    </div>
+  );
+}
