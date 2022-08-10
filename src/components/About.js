c5bea81bfd294b3b9dddfb0315123b5e7fc66c91
Brooke Love <savienloveb@gmail.com>
2022-08-10 12:25:32 -0700

    adding in photos to use

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "../assets/css/components/About.css";
-
+import UncleJ from "../assets/images/UncleJ.png";
 export default function About() {
   return (
     <div className="aboutContainer">
-      <h1 id="about">About Me</h1>
-      <p>
-        I am a one man machine that creates any cake that creates new invidint
-        deserts.
-      </p>
-      <p>If you cna think it I can do it!</p>
+      <h1 id="about" className="aboutTitle">
+        CAKE MAN
+      </h1>
+      <div className="trifold">
+        <img src={UncleJ} width="300" className="uncleJPic" />
+        <div className="textContainer">
+          <p className="aboutText">
+            I am a one man machine that creates any cake that creates new
+            inventive deserts.
+          </p>
+          <p className="aboutText">If you can think it I can do it!</p>
+          <p>Based out of Florida</p>
+        </div>
+      </div>
     </div>
   );
 }
